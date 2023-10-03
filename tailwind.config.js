c41d6aec1fe9ef0f87262f34d02d44254426e305
Bibhu <100408110+DeBibhu562@users.noreply.github.com>
2023-10-03 18:51:59 +0530

    About page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'lightcard': 'rgb(245, 247, 253)',
+        'midnightBlack': 'rgb(26, 29, 45)',
+      },
+    },
     screens: {
-			sm: "640px",
-			md: "768px",
-			lg: "1024px",
-			xl: "1280px",
-		},
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 }
