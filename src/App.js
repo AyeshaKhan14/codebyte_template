520a1d1416519dbb99ace3a687e43d5b99deb140
Ayeshakhan14 <ayeshatravels9@gmail.com>
2023-10-03 19:01:54 +0530

    error correction

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@
 import './App.css';
 import { Nav } from './Components/NavBar/Nav';
 import { AllRoutes } from './Pages/AllRoutes';
-import { Contact } from './Pages/Contact';
 import { Footer } from './Pages/Footer';
 
 function App() {
   return (
     <>
       <Nav />
-      {/* <AllRoutes /> */}
-      <Contact />
+      <AllRoutes />
       <Footer/>
     </>
   );
